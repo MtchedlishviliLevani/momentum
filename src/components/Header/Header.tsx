31a284f554a79e66e0a430755f0cc78efa85987f
MtchedlishviliLevani <levan.mtchedlishvili.5@iliauni.edu.ge>
2025-03-19 22:48:06 +0400

    feat: add navigation to header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,21 @@
+import { Link, useNavigate } from "react-router"
 import Button from "../UI/Button"
 
 function Header({ onOpenModal }: { onOpenModal: () => void }) {
+    const navigate = useNavigate();
+    function openCreateNewTask() {
+        navigate("/create-task")
+    }
     return (
         <header className="py-[31px]">
             <div className="flex justify-between items-center">
-                <img src="/svg/logo1.svg" alt="page's logo" />
+                <Link to={"/"}> <img src="/svg/logo1.svg" alt="page's logo" />   </Link>
                 <div className="flex items-center gap-[40px]">
-                    <Button variant="outline" shape="default" onClick={onOpenModal}>თანამშრომლის შექმნა</Button>
-                    <Button variant="primary" shape="default"><img src="/svg/addIcon.svg" alt="add Icon" /><span>შექმენი ახალი დავალება</span></Button>
+                    <Button variant="outline" shape="default" padding="compact" onClick={onOpenModal}>თანამშრომლის შექმნა</Button>
+                    <Button variant="primary" shape="default" padding="compact" onClick={openCreateNewTask}><img src="/svg/addIcon.svg" alt="add Icon" /><span>შექმენი ახალი დავალება</span></Button>
                 </div>
             </div>
-        </header>
+        </header >
     )
 }
 
